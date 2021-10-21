54cba39b7819b2cd967766bf8c3ecb1224ef6903
AmNaqib <naqib.zaidi@pipeline-network.com>
2021-10-21 12:15:16 +0800

    req sent http
    Mixed content blocked

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="theme-color" content="#ffffff">
     <meta name="csrf-token" content="{{ csrf_token() }}" />
+    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests"> 
     <title>RISDA | Keusahawanan</title>
     
     <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
