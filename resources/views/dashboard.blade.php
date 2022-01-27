be0031b5f5acbecc3c7998d9865e4efd29174e7a
AmNaqib <naqib.zaidi@pipeline-network.com>
2022-01-28 03:32:54 +0800

    Update dashboard.blade.php

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -299,7 +299,7 @@
           <nav class="navbar navbar-light navbar-glass navbar-top navbar-expand">
 
             <button class="btn navbar-toggler-humburger-icon navbar-toggler me-1 me-sm-3" type="button" data-bs-toggle="collapse" data-bs-target="#navbarVerticalCollapse" aria-controls="navbarVerticalCollapse" aria-expanded="false" aria-label="Toggle Navigation"><span class="navbar-toggle-icon"><span class="toggle-line"></span></span></button>
-            <a class="navbar-brand me-1 me-sm-3" href="/">
+            <a class="navbar-brand me-1 me-sm-3" href="/landing">
               <div class="d-flex align-items-center"><img class="me-2" src="../../../assets/img/risda.png" alt="" width="40" /><span class="font-sans-serif">RISDA</span>
               </div>
             </a>
