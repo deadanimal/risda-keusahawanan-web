305ab29bbf323800de1ac062aacb658d8db81eb7
AmNaqib <naqib.zaidi@pipeline-network.com>
2022-01-28 02:15:09 +0800

    Update LapProf.php

--- app/Exports/LapProf.php
+++ app/Exports/LapProf.php
@@ -132,21 +132,19 @@ class LapProf implements FromArray, WithHeadings
             $usahawan->AlamatMediumPemasaran = "";
 
             if(isset($usahawan->perniagaan)){
-                if(isset($usahawan->perniagaan->facebook)){
-                    if($usahawan->perniagaan->facebook != ""){
-                        $usahawan->MediumPemasaran .= "Facebook ";
-                        $usahawan->AlamatMediumPemasaran .= "Facebook - ".$usahawan->perniagaan->facebook;
-                    }
-                }
                 
-                // if($usahawan->perniagaan->instagram != ""){
-                //     $usahawan->MediumPemasaran .= "Instagram ";
-                //     $usahawan->AlamatMediumPemasaran .= "Instagram - ".$usahawan->perniagaan->instagram;
-                // }
-                // if($usahawan->perniagaan->twitter != ""){
-                //     $usahawan->MediumPemasaran .= "Twitter ";
-                //     $usahawan->AlamatMediumPemasaran .= "Twitter - ".$usahawan->perniagaan->twitter;
-                // }
+                if($usahawan->perniagaan->facebook != ""){
+                    $usahawan->MediumPemasaran = $usahawan->MediumPemasaran."Facebook ";
+                    $usahawan->AlamatMediumPemasaran = $usahawan->AlamatMediumPemasaran."Facebook - ".$usahawan->perniagaan->facebook;
+                }
+                if($usahawan->perniagaan->instagram != ""){
+                    $usahawan->MediumPemasaran = $usahawan->MediumPemasaran."Instagram ";
+                    $usahawan->AlamatMediumPemasaran = $usahawan->AlamatMediumPemasaran."Instagram - ".$usahawan->perniagaan->instagram;
+                }
+                if($usahawan->perniagaan->twitter != ""){
+                    $usahawan->MediumPemasaran = $usahawan->MediumPemasaran."Twitter ";
+                    $usahawan->AlamatMediumPemasaran = $usahawan->AlamatMediumPemasaran."Twitter - ".$usahawan->perniagaan->twitter;
+                }
             }
             
             // if(isset($usahawan->syarikat)){
